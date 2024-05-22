945a4f0cf7d85de6495e168e2133d0eec593af00
ahed1854 <12345makemoney12345@gmail.com>
2024-05-21 21:04:26 -0500

    Made the page redirect automaticaly

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ let seconds = 10;
 function countdown() {
   seconds = seconds - 1;
   if (seconds < 0) {
-    document.getElementById("redirectButton").disabled = false;
-    console.log("You'll be able to get to the page !!");
+    console.log("you'le be redirected !!");
+    // window.location("https://www.google.com");
   } else {
     document.getElementById("countdown").innerHTML = seconds;
     window.setTimeout("countdown()", 1000);
@@ -12,8 +12,8 @@ function countdown() {
 
 countdown();
 
-document
-  .getElementById("redirectButton")
-  .addEventListener("click", function () {
-    window.open("https://www.google.com", "_blank");
-  });
+// document
+//   .getElementById("redirectButton")
+//   .addEventListener("click", function () {
+//     window.open("https://www.google.com", "_blank");
+//   });
